9750e76c251e5dd0c004289b65e50b0be5f3e8e2
Vibhu rathore <79627056+Vibhurathore09@users.noreply.github.com>
2021-03-22 15:17:09 +0530

    leap year

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,6 @@
-def is_leap(year):
-    return year%4==0 and (year%100!=0 or year%400==0)
+yer = int(input())
+if yer%4==0 and yer%100!=0 or yer%400==0:
+    print("leap year")
+else:
+    print("Not leap year")
+    
